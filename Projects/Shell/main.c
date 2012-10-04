95846be712f299a4de4765b05e4fed0937fee5f9
Curt <cwmahoney@colgate.edu>
2012-10-04 19:02:51 -0400

    Argh

--- Projects/Shell/main.c
+++ Projects/Shell/main.c
@@ -41,21 +41,47 @@ void killComments(char *input,int len){
 struct node {
     pid_t proc;
     struct node *next; 
+	struct node *last; //need it for removal without stupidity
 };
 
 void insert(pid_t proc_id, struct node **head) {
     struct node *newnode = malloc(sizeof(struct node));
+	newnode->last = NULL;
     newnode->next = *head;
+	if(*head!=NULL){
+		(*head)->last = newnode;
+	}
+
 	newnode->proc = proc_id;
     *head = newnode;
 }
 
 void clear_list(struct node *curnode) {
+	struct node *tmp;
 	while (curnode != NULL) {
-		struct node *tmp = curnode;
+		tmp = curnode;
 		curnode = curnode->next;
 		free(tmp);
 	}
+}
+
+void killNode(struct node *curnode, struct node **head){ //Curt's //still no good - mode p \n ls;ls;sleep 2;ls;mode s; ls
+	struct node *tmp = curnode->last;
+
+	if(*head==curnode){ //curnode is head
+		tmp=curnode->next;
+		if(tmp!=NULL){	 //head is not alone		
+			tmp->last=NULL;
+			*head=tmp;
+		}
+	}else if(curnode->next!=NULL){ //curnode is in middle somewhere
+		tmp->next = curnode->next;
+		curnode->next = curnode->last;
+	}else{ //tail of list
+		tmp = curnode->last;
+		tmp->next=NULL;
+	}
+	free(curnode);
 }
 
 
@@ -201,6 +227,49 @@ char modeCheck(char **cmd, char mode){
 	return 'n'; //an int, not a bool
 }
 
+/*Builds an array of all the valid paths to search, manually input by programemr*/
+char **buildPaths(){
+	char **paths = malloc(sizeof(char *)*8); //seven paths to test, then NULL
+
+	paths[0] = strdup("/bin/");
+	paths[1] = strdup("/usr/sbin/");
+	paths[2] = strdup("/sbin/");
+	paths[3] = strdup("/usr/local/bin/");
+	paths[4] = strdup("./");
+	paths[5] = strdup("/usr/games/");
+	paths[6] = strdup("/usr/bin/");
+	paths[7] = NULL;
+
+	return paths; //7 malloced strings and 1 malloced string of strings
+	
+}
+
+int testCmdReal(char **instr,char **paths, int max_len){
+	char temp_c[max_len*2]; //covers all string lens possible
+	int i = 0;
+
+	struct stat statresult;
+	int rv;
+
+	rv = stat(*instr, &statresult );
+	if(rv>=0){
+		return 1; //original command was valid
+	}
+
+	for(;i<arrLen(paths)-1;i++){
+		strcpy(temp_c,paths[i]);
+		strcat(temp_c,*instr);	
+		printf("TESTING _%s_\n",temp_c);
+		rv = stat(temp_c, &statresult );
+		if(rv>=0){ //stat succeeded
+			free(*instr);
+			*instr=strdup(temp_c);
+			return 1; //success
+		}
+	}
+	return 0; //no valid path found
+}
+
 //Need to deal with string-literal input, parallel vs. sequential, user vs. kernal time, EOF-checkery
 int main(int argc, char **argv){
     char *prompt = "CMAS> ";
@@ -211,6 +280,9 @@ int main(int argc, char **argv){
 	buffer[0]='\0'; //initial value
 	char **cmd_arr;
 	char mode = 's'; //Mode "bit." 's' means sequential
+
+	struct node *kids = NULL; //for tracking running processes
+	char **paths = buildPaths();		
 
     while (fgets(buffer, buffer_len, stdin) != NULL) { //works as an EOF checker, according to Prof.
         /* process current command line in buffer */
@@ -223,7 +295,7 @@ int main(int argc, char **argv){
 		char ***clean_cmd_arr=malloc(sizeof(char **)*arrLen(cmd_arr));
 		char **temp_c;
 
-		printf("Length of cmd_arr: %d\n",arrLen(cmd_arr));
+		//printf("Length of cmd_arr: %d\n",arrLen(cmd_arr));
 
 		while(cmd_arr[i]!=NULL){
 			temp_c=breakCommand(cmd_arr[i]); //malloced, remember
@@ -236,7 +308,7 @@ int main(int argc, char **argv){
 		free(cmd_arr);
 		clean_cmd_arr[clean_len]=NULL;
 		
-		printf("cca len: %d\n",arrLen2(clean_cmd_arr));
+		//printf("cca len: %d\n",arrLen2(clean_cmd_arr));
         
         //char *cmd[] = { "/bin/ls", "-l","-t", "-r", ".", NULL };
 
@@ -245,7 +317,7 @@ int main(int argc, char **argv){
 		char temp_m;
 		int j;
 		_Bool will_exit = 0;
-		struct node *kids = NULL;
+		pid_t p;
 		for(;clean_cmd_arr[i]!=NULL;i++)  //i < length of clean_cmd_array
 		{
 			cmd = clean_cmd_arr[i];
@@ -254,15 +326,24 @@ int main(int argc, char **argv){
 			if(0==strcmp(cmd[0],"exit")){ //exit command given
 				will_exit = 1; //will exit later
 			}else
-			{	 //not an "exit" command
+			{ //mode checking
 				temp_m = modeCheck(cmd,mode);
 				if('n'!=temp_m){
 					mode=temp_m;
-				}else //no "mode" starting cmd
+				}else
 				{
-					insert(execCmd(cmd, mode), &kids);
+					if((cmd[0]==NULL)||(0==testCmdReal(&cmd[0],paths,buffer_len))){ //returns a 0 or 1, changes cmd[0] if real command
+						printf("Execution of %s failed, invalid file\n",cmd[0]);
+					}else
+					{
+						p = execCmd(cmd, mode); //p>0 if a child is still alive at the end of execCmd
+						if(p>0){ //only add living (to our knowledge) children to the linked list
+							insert(p, &kids);
+						}						
+					}
 				}
 			}
+
 			j=0;
 			for(;j<arrLen(clean_cmd_arr[i]);j++){
 				free(clean_cmd_arr[i][j]);
@@ -271,12 +352,14 @@ int main(int argc, char **argv){
 		}
 		//after all commands executed
 	
-		struct node *copy = NULL;	
-		while (copy != NULL) {
+		struct node *copy = kids; //start at head of list
+		struct node *tmp;
+		while (copy != NULL){
 			waitpid(copy->proc, NULL, 0);
+			tmp=copy;
 			copy = copy->next;
+			killNode(tmp,&kids); //removes node of dead child
 		}
-		clear_list(kids);
 		free(copy);
 		free(clean_cmd_arr);
 
@@ -287,13 +370,18 @@ int main(int argc, char **argv){
 			printf("%ld.%06ld seconds spent in user mode\n", usage_self.ru_utime.tv_sec + usage_children.ru_utime.tv_sec, usage_self.ru_utime.tv_usec + usage_children.ru_utime.tv_usec);
 			printf("%ld.%06ld seconds spent in kernel mode\n", usage_self.ru_stime.tv_sec + usage_children.ru_stime.tv_sec, usage_self.ru_stime.tv_usec + usage_children.ru_stime.tv_usec);
 		
-			break; //leave while loop
+			break; //leave while-loop
 		}
-
 		printf("%s", prompt);
 		fflush(stdout);
 		//printf("TESTING\n");
     }
+	clear_list(kids);
+	int k = 0;
+	for(;k<arrLen(paths);k++){
+		free(paths[k]);
+	}
+	free(paths);
 	free(buffer);
 
     return 0;
