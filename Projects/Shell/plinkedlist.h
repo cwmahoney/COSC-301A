3d2e486b0ebb66079381f4718ac67a7a6bd4d497
Curt Mahoney <cwmahoney@colgate.edu>
2012-10-06 18:06:12 -0300

    Update Projects/Shell/plinkedlist.h

--- Projects/Shell/plinkedlist.h
+++ Projects/Shell/plinkedlist.h
@@ -7,7 +7,5 @@ struct node {
 };
 
 void insert(char *cmd, pid_t proc_id, struct node **head);
-/*Wipes out the entire list after curnode*/
 void clear_list(struct node *curnode);
-/*removes a node from the ll starting at head*/
 void killNode(struct node *curnode, struct node **head);
